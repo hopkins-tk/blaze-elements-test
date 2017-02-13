4df4461cb0cc20be7702472955fae2905b92a240
tomas.kral <tomas.kral@embedit.cz>
2017-02-13 14:52:34 +0100

    chore(calendar): add button dependency

--- packages/calendar/components/Button.tsx
+++ packages/calendar/components/Button.tsx
@@ -1,6 +1,6 @@
 import { define } from 'skatejs';
 // @FIXME this needs to be imoprted from 'bl-button' package, not relatively
-import { Button } from '../../button/Button';
+import { Button } from '@blaze-elements/button';
 
 export class CalendarButton extends Button {
   static get is() { return 'bl-calendar-button'; }
